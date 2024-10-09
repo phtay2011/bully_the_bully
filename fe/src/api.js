c5b44ce77fd71ba68d5e231c642186f327cef27b
paul <paul@open.gov.sg>
2024-10-09 14:59:38 +0800

    make api baseurl an env

--- fe/src/api.js
+++ fe/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/api";
+const API_URL = process.env.REACT_APP_API_URL;
 // const API_URL =
 //   "http://ec2-54-254-230-174.ap-southeast-1.compute.amazonaws.com:3001/api";
 
